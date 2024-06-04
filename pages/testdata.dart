e4a4ac13e15c81d9d537f0711b2e3e479d4947a8
pandamin24 <135835977+pandamin24@users.noreply.github.com>
2024-06-04 12:55:42 +0900

    Add files via upload

---
+++ pages/testdata.dart
@@ -0,0 +1,379 @@
+import 'constants.dart';
+
+List<Map<String, dynamic>> testAllRequireList = [
+  {
+    //list item titie = caretype
+    //시간 추가
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.boarding,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종3',
+    "careType": CareType.grooming,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.walking,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종3',
+    "careType": CareType.boarding,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.boarding,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종2',
+    "careType": CareType.etc,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+  {
+    "id": '등록 id',
+    "image": '애견 이미지',
+    "breed": '견종1',
+    "careType": CareType.playtime,
+    "status": '등록 상태'
+  },
+];
+
+List<Map<String, dynamic>> testMyRequireList = [
+  {
+    "id": '1',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '2',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '3',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '4',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '5',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '6',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '7',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '8',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+  {
+    "id": '9',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '등록 상태',
+  },
+];
+
+List<Map<String, dynamic>> testMyApplyList = [
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+  {
+    "id": '신청 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '신청 상태',
+  },
+];
+
+List<Map<String, dynamic>> testMyMatchLogList = [
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+  {
+    "id": '매칭 id',
+    "image": '애견 이미지',
+    "breed": '견종',
+    "careType": '케어 타입',
+    "status": '매칭 상태',
+  },
+];
+
+List<Map<String, dynamic>> testDogList = [
+  {'id': 'a', 'name': 'dog1', 'gender': 'boy', 'image': 'else'},
+  {'id': 'b', 'name': 'dog2', 'gender': 'girl', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+  {'id': 'c', 'name': 'dog3', 'gender': 'boy', 'image': 'else'},
+]; //표면정보만 가져와서 리스트뷰로 출력. 상세 정보는 클릭 시 출력
+
